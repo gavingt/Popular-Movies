6c907aab2307cae72910787a94dcf40da3a1db20
Gavin Wright <gavingt@gmail.com>
2016-12-25 04:29:27 -0700

    Details page done, app only working on emulator

--- app/src/main/java/com/example/gavin/movies/DetailsFragment.java
+++ app/src/main/java/com/example/gavin/movies/DetailsFragment.java
@@ -4,13 +4,19 @@ package com.example.gavin.movies;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import jp.wasabeef.picasso.transformations.ColorFilterTransformation;
+
+import static android.os.Build.VERSION_CODES.M;
 
 
 /**
@@ -18,15 +24,10 @@ import com.squareup.picasso.Picasso;
  */
 public class DetailsFragment extends Fragment {
 
-    public String mMoviePosterUrl;
-    public String mTitle;
-    public double mPopularity;
-    public double mUserRating;
-    public String mPlotSynopsis;
-    public String mReleaseDate;
-    public String mBackdropImageUrl;
+    private final String LOG_TAG = DetailsFragment.class.getSimpleName();
 
-    private final String URL_PREFIX = "http://image.tmdb.org/t/p/w780";
+    private final String BACKGROUND_URL_PREFIX = "http://image.tmdb.org/t/p/w780";
+    private final String POSTER_URL_PREFIX = "http://image.tmdb.org/t/p/w185";
 
 
     @Override
@@ -37,15 +38,31 @@ public class DetailsFragment extends Fragment {
         Movie currentMovie = (Movie) getActivity().getIntent().getSerializableExtra("movieObject");
 
         ImageView backgroundImage = (ImageView) rootView.findViewById(R.id.background_image);
-        //backgroundImage.setColorFilter(Color.argb(150, 0, 0, 50));
-        Picasso.with(getContext()).load(URL_PREFIX + currentMovie.mBackdropImageUrl).into(backgroundImage);
+        ColorFilterTransformation transformation = new ColorFilterTransformation(0x96000032);
+        Picasso.with(getContext()).load(BACKGROUND_URL_PREFIX + currentMovie.mBackdropImageUrl).transform(transformation).into(backgroundImage);
 
+        ImageView moviePoster = (ImageView) rootView.findViewById(R.id.movie_poster);
+        Picasso.with(getContext()).load(POSTER_URL_PREFIX + currentMovie.mMoviePosterUrl).into(moviePoster);
 
+        getActivity().setTitle(currentMovie.mTitle);
 
+        RatingBar ratingBar = (RatingBar) rootView.findViewById(R.id.movie_rating);
+        ratingBar.setRating((float) (currentMovie.mUserRating/2));
 
+        TextView plotSynopsis = (TextView) rootView.findViewById(R.id.movie_synopsis);
+        plotSynopsis.setText(currentMovie.mPlotSynopsis);
+
+        TextView releaseDate = (TextView) rootView.findViewById(R.id.movie_release_date);
+        releaseDate.setText("Release: " + formatDate(currentMovie));
 
 
         return rootView;
+    }
+
+    private String formatDate(Movie currentMovie) {
+        String unformattedDate = currentMovie.mReleaseDate;
+        String[] twoPieces = unformattedDate.split("-");
+        return twoPieces[1] + "/" + twoPieces[2] + "/" + twoPieces[0];
     }
 
 }
